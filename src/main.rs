b3165a12b1baa372b101056c4e05b8128ba7ecae
paul wisehart <paul@oldcode.org>
2019-08-25 20:43:42 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
-	// 216.52.148.134:27961 urtctf
-	//let mut _socket1 = UdpSocket::bind("216.52.148.134:27961").expect("couldnt bind pkw");
-	//let mut _socket2 = UdpSocket::bind("216.52.148.134:27961")?;
-	let mut _socket = UdpSocket::bind("127.0.0.1:34254")?;
+    // 216.52.148.134:27961 urtctf
+    //let mut _socket1 = UdpSocket::bind("216.52.148.134:27961").expect("couldnt bind pkw");
+    //let mut _socket2 = UdpSocket::bind("216.52.148.134:27961")?;
+    let socket = UdpSocket::bind("127.0.0.1:34254")?;
+    socket.connect("216.52.148.134:27961")?;
 
-	Ok(())
+    Ok(())
 }
