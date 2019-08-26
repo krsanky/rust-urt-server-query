5e05800daf75436da5d0fb545760e50a927104ab
paul wisehart <paul@oldcode.org>
2019-08-25 20:39:59 -0400

    baby steps into udp sockets w/rust ....

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+	// 216.52.148.134:27961 urtctf
+	//let mut _socket1 = UdpSocket::bind("216.52.148.134:27961").expect("couldnt bind pkw");
+	//let mut _socket2 = UdpSocket::bind("216.52.148.134:27961")?;
+	let mut _socket = UdpSocket::bind("127.0.0.1:34254")?;
+
+	Ok(())
 }
